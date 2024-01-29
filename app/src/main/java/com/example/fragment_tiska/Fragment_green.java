6bd68135a8bee0aeb6f113629aa01edc9851fe00
uswtnnisa <uswatunnisa382@gmail.com>
2024-01-29 11:11:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fragment_tiska/Fragment_green.java
@@ -0,0 +1,6 @@
+package com.example.fragment_tiska;
+
+import android.app.Activity;
+
+public class Fragment_green extends Activity {
+}
---
+++ app/src/main/java/com/example/fragment_tiska/Fragment_red.java
@@ -0,0 +1,6 @@
+package com.example.fragment_tiska;
+
+import android.app.Activity;
+
+public class Fragment_red extends Activity {
+}
